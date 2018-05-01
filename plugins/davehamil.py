2a770fa4a46f4d8e4dc56c423ed8256ae9262cf2
Rory Holland <github@rory.sh>
2018-05-01 11:28:56 +0100

    Use Spacy to perform natural language processing. Plus some text hacks.

--- plugins/davehamil.py
+++ plugins/davehamil.py
@@ -5,12 +5,24 @@ import random
 import asyncio
 import re
 import markovify
+import spacy
 from random import randint
 
 from cloudbot import hook
 from cloudbot.util import textgen
 
 nick_re = re.compile("^[A-Za-z0-9_|.\-\]\[\{\}\*]*$", re.I)
+
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+
+class POSifiedText(markovify.Text):
+    def word_split(self, sentence):
+        return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
+
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
 
 @hook.on_start()
 def load_text(bot):
@@ -23,13 +35,16 @@ def load_text(bot):
         # Get raw text as string.
         text = f.read()
     # Build the model.
-    text_model = markovify.Text(text)
+    text_model = POSifiedText(text)
 
 @asyncio.coroutine
 @hook.command
 def dh(text, message):
 
-    phrase = text_model.make_short_sentence(randint(30,300))
+    phrase = text_model.make_sentence().replace(" '", "'").replace(" .", ".").replace(" !", "!").replace(" ?", "?").replace(" ,", ",")
+    if len(phrase) < 60:
+        phrase = phrase + " " + text_model.make_sentence().replace(" '", "'").replace(" .", ".").replace(" !", "!").replace(" ?", "?").replace(" ,", ",")
+    
     # act out the message
     message(phrase)
 
